66132e0b89c4f48369c6ed6169830bbcc74b1698
shakeeb <shakeebanwar250@gmail.com>
2020-10-27 23:57:34 +0500

    done

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -7,7 +7,132 @@ function HomePage(){
         <div className = "home-page">
 
             <img className = "slider" src={sliderPic} alt=""/>
+
+            <br/>
+            <br/>
+            <div className="container">
+
+
+                <h4>Fresh recommendations</h4>
+                {/* <div className="row">
+
+                <div className="col">1 of 4</div>
+                <div className="col">2 of 4</div>
+                <div className="col">3 of 4</div>
+                <div className="col">4 of 4</div>
+                </div> */}
+
+<div>
+        <div className="container text-center">
+          
         </div>
+        <div className="shell">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-3">
+                <div className="wsk-cp-product">
+                  <div className="wsk-cp-img">
+                    <img src="https://3.bp.blogspot.com/-eDeTttUjHxI/WVSvmI-552I/AAAAAAAAAKw/0T3LN6jABKMyEkTRUUQMFxpe6PLvtcMMwCPcBGAYYCw/s1600/001-culture-clash-matthew-gianoulis.jpg" alt="Product" className="img-responsive" />
+                  </div>
+                  <div className="wsk-cp-text">
+                    <div className="category">
+                    <span className = "featured">Featured</span>
+                    </div>
+                    <div className="title-product">
+                      <h3>My face not my heart</h3>
+                    </div>
+                    <div className="description-prod">
+                      <p>Description Product tell me how to change playlist height size like 600px in html5 player. player good work now check this link</p>
+                    </div>
+                    <div className="card-footer">
+                      <div className="wcf-left"><span className="price">Rp500.000</span></div>
+                      <div className="wcf-right">
+
+
+                      <svg width="24px" height="24px" viewBox="0 0 1024 1024" data-aut-id="icon" class="" fill-rule="evenodd"><path class="rui-77aaa" d="M830.798 448.659l-318.798 389.915-317.828-388.693c-20.461-27.171-31.263-59.345-31.263-93.033 0-85.566 69.605-155.152 155.152-155.152 72.126 0 132.752 49.552 150.051 116.364h87.777c17.299-66.812 77.905-116.364 150.051-116.364 85.547 0 155.152 69.585 155.152 155.152 0 33.687-10.802 65.862-30.293 91.811zM705.939 124.121c-80.853 0-152.204 41.425-193.939 104.204-41.736-62.778-113.086-104.204-193.939-104.204-128.33 0-232.727 104.378-232.727 232.727 0 50.657 16.194 98.948 47.806 140.897l328.766 402.133h100.189l329.716-403.355c30.662-40.727 46.856-89.018 46.856-139.675 0-128.349-104.398-232.727-232.727-232.727z"></path></svg>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div className="wsk-cp-product">
+                  <div className="wsk-cp-img"><img src="https://1.bp.blogspot.com/-b-2SgNUrFHg/WVSvmewWqgI/AAAAAAAAAK0/1K4YCcbYjhokHwV_IgiVJN9mEnQoWunIwCPcBGAYYCw/s1600/fashion-portrait-nicole-6347.jpg" alt="Product" className="img-responsive" /></div>
+                  <div className="wsk-cp-text">
+                    <div className="category">
+                    <span className = "featured">Featured</span>
+                    </div>
+                    <div className="title-product">
+                      <h3>My face not my heart</h3>
+                    </div>
+                    <div className="description-prod">
+                      <p>Description Product tell me how to change playlist height size like 600px in html5 player. player good work now check this link</p>
+                    </div>
+                    <div className="card-footer">
+                      <div className="wcf-left"><span className="price">Rp500.000</span></div>
+                      <div className="wcf-right">
+
+                      <svg width="24px" height="24px" viewBox="0 0 1024 1024" data-aut-id="icon" class="" fill-rule="evenodd"><path class="rui-77aaa" d="M830.798 448.659l-318.798 389.915-317.828-388.693c-20.461-27.171-31.263-59.345-31.263-93.033 0-85.566 69.605-155.152 155.152-155.152 72.126 0 132.752 49.552 150.051 116.364h87.777c17.299-66.812 77.905-116.364 150.051-116.364 85.547 0 155.152 69.585 155.152 155.152 0 33.687-10.802 65.862-30.293 91.811zM705.939 124.121c-80.853 0-152.204 41.425-193.939 104.204-41.736-62.778-113.086-104.204-193.939-104.204-128.33 0-232.727 104.378-232.727 232.727 0 50.657 16.194 98.948 47.806 140.897l328.766 402.133h100.189l329.716-403.355c30.662-40.727 46.856-89.018 46.856-139.675 0-128.349-104.398-232.727-232.727-232.727z"></path></svg>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div className="wsk-cp-product">
+                  <div className="wsk-cp-img"><img src="https://1.bp.blogspot.com/-XL_Ba-178Fo/WVSvm5AbApI/AAAAAAAAAK4/X5109HTqUiAhPjbmz4NFVHcpL7ZWe6T3ACPcBGAYYCw/s1600/wow-29.jpg" alt="Product" className="img-responsive" /></div>
+                  <div className="wsk-cp-text">
+                    <div className="category">
+                    <span className = "featured">Featured</span>
+                    </div>
+                    <div className="title-product">
+                      <h3>My face not my heart</h3>
+                    </div>
+                    <div className="description-prod">
+                      <p>Description Product tell me how to change playlist height size like 600px in html5 player. player good work now check this link</p>
+                    </div>
+                    <div className="card-footer">
+                      <div className="wcf-left"><span className="price">Rp500.000</span></div>
+                      <div className="wcf-right">
+                      <svg width="24px" height="24px" viewBox="0 0 1024 1024" data-aut-id="icon" class="" fill-rule="evenodd"><path class="rui-77aaa" d="M830.798 448.659l-318.798 389.915-317.828-388.693c-20.461-27.171-31.263-59.345-31.263-93.033 0-85.566 69.605-155.152 155.152-155.152 72.126 0 132.752 49.552 150.051 116.364h87.777c17.299-66.812 77.905-116.364 150.051-116.364 85.547 0 155.152 69.585 155.152 155.152 0 33.687-10.802 65.862-30.293 91.811zM705.939 124.121c-80.853 0-152.204 41.425-193.939 104.204-41.736-62.778-113.086-104.204-193.939-104.204-128.33 0-232.727 104.378-232.727 232.727 0 50.657 16.194 98.948 47.806 140.897l328.766 402.133h100.189l329.716-403.355c30.662-40.727 46.856-89.018 46.856-139.675 0-128.349-104.398-232.727-232.727-232.727z"></path></svg>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div className="wsk-cp-product">
+                  <div className="wsk-cp-img"><img src="https://3.bp.blogspot.com/-iUes8qr4uC8/WVSvl6Wf_fI/AAAAAAAAAKs/JiTUUVvdbqEC_QGGaQhuVJiezIN3LIqEgCPcBGAYYCw/s1600/IMG_5367-bp.jpg" alt="Product" className="img-responsive" /></div>
+                  <div className="wsk-cp-text">
+                    <div className="category">
+                      <span className = "featured">Featured</span>
+                    </div>
+                    <div className="title-product">
+                      <h3>My face not my heart cvf ggf gfg g</h3>
+                    </div>
+                    <div className="description-prod">
+                      <p>Description Product tell me how to change playlist height size like 600px in html5 player. player good work now check this link</p>
+                    </div>
+                    <div className="card-footer">
+                      <div className="wcf-left"><span className="price">Rp500.000</span></div>
+                      <div className="wcf-right">
+
+                          <svg width="24px" height="24px" viewBox="0 0 1024 1024" data-aut-id="icon" class="" fill-rule="evenodd"><path class="rui-77aaa" d="M830.798 448.659l-318.798 389.915-317.828-388.693c-20.461-27.171-31.263-59.345-31.263-93.033 0-85.566 69.605-155.152 155.152-155.152 72.126 0 132.752 49.552 150.051 116.364h87.777c17.299-66.812 77.905-116.364 150.051-116.364 85.547 0 155.152 69.585 155.152 155.152 0 33.687-10.802 65.862-30.293 91.811zM705.939 124.121c-80.853 0-152.204 41.425-193.939 104.204-41.736-62.778-113.086-104.204-193.939-104.204-128.33 0-232.727 104.378-232.727 232.727 0 50.657 16.194 98.948 47.806 140.897l328.766 402.133h100.189l329.716-403.355c30.662-40.727 46.856-89.018 46.856-139.675 0-128.349-104.398-232.727-232.727-232.727z"></path></svg>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+
+          </div>
+        </div>
+      </div>
+            </div>
+        </div>
+
+
 
 
 
